2bd9d75e3c6b35cf2f8764b96fd84ad7b512a19d
Jobe <patchbatch9@gmail.com>
2023-05-31 23:58:05 +0100

    Implemented Real time Gps tracking

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
@@ -17,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'YO ITYS YA BOIY'),
     );
   }
 }
@@ -31,15 +33,23 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _counter = "";
-  String long = "", lat = "";
+  String long = "", lat = "", Distance = "";
   void _incrementCounter() {}
 
   _determinePosition() async {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-    long = position.longitude.toString();
-    lat = position.latitude.toString();
+    final LocationSettings locationSettings = LocationSettings(
+      accuracy: LocationAccuracy.high,
+      distanceFilter: 1,
+    );
+    StreamSubscription<Position> positionStream =
+        Geolocator.getPositionStream(locationSettings: locationSettings)
+            .listen((Position? position) {
+      long = position!.longitude.toString();
+      lat = position!.latitude.toString();
+      print(long);
+
+      setState(() {});
+    });
     setState(() {});
   }
 
@@ -52,34 +62,21 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text("Longitude: $long", style: TextStyle(fontSize: 20)),
-            Text(
-              "Latitude: $lat",
-              style: TextStyle(fontSize: 20),
-            ),
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+              Text("Distance Travelled: $Distance",
+                  style: TextStyle(fontSize: 20)),
+              Text("Longitude: $long", style: TextStyle(fontSize: 20)),
+              Text(
+                "Latitude: $lat",
+                style: TextStyle(fontSize: 20),
+              ),
+            ])));
   }
 }
