343e9cce69adab94dcb133b3b5ef0cf7ad47355d
Jobe <patchbatch9@gmail.com>
2023-06-01 18:30:16 +0100

    added in M

--- lib/DistanceCalculation.dart
+++ lib/DistanceCalculation.dart
@@ -6,18 +6,21 @@ List<Position> Location = [];
 String long = "", lat = "", distance = "";
 determinePosition() async {
   Position _previousPosition;
+  double FinalDistance = 0;
   final LocationSettings locationSettings = const LocationSettings(
     accuracy: LocationAccuracy.high,
     distanceFilter: 1,
   );
+  // ignore: unused_local_variable
   StreamSubscription<Position> positionStream =
       Geolocator.getPositionStream(locationSettings: locationSettings)
           .listen((Position? position) {
     Location.add(position!);
     _previousPosition = Location.elementAt(Location.length - 2);
-    double distance = calculateDistance(_previousPosition.latitude,
+    double distanceD = calculateDistance(_previousPosition.latitude,
         _previousPosition.longitude, position.latitude, position.longitude);
-    print(distance);
+    FinalDistance = FinalDistance + distanceD;
+    print(FinalDistance);
   });
 }
 
@@ -27,5 +30,5 @@ double calculateDistance(lat1, lon1, lat2, lon2) {
   var a = 0.5 -
       c((lat2 - lat1) * p) / 2 +
       c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p)) / 2;
-  return 12742 * asin(sqrt(a));
+  return 1000 * 12742 * asin(sqrt(a));
 }
